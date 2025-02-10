10c4180da93e6e3e7b638f525c2bc184d39012dd
mikoofiaza@gmail.com <mikoofiaza@gmail.com>
2025-02-10 16:33:54 +0800

    modified:   src/app/globals.css 	modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,6 +29,58 @@ export default function Home() {
             />
           </div>  
         </div>
+
+        <div className="bg-gray-700">
+          <h1 className="text-white text-center text-5xl font-bold mt-8">Skills</h1>
+            <div className="grid grid-cols-2 p-8">
+              <div
+                className="block rounded-lg p-6 text-surface shadow-secondary-1 dark:bg-surface-dark text-white bg-gray-700 border border-gray-500">
+                <h5 className="mb-2 text-xl font-medium leading-tight">Angular</h5>
+                <p className="mb-4 text-base">
+                  I specialize in Angular development, building dynamic, responsive, and scalable web applications with a focus on performance, maintainability, and seamless user experiences.
+                </p> 
+              </div>
+              <div
+                className="block rounded-lg p-6 text-surface shadow-secondary-1 dark:bg-surface-dark text-white bg-gray-700 border border-gray-500">
+                <h5 className="mb-2 text-xl font-medium leading-tight">MySQL</h5>
+                <p className="mb-4 text-base">
+                  Proficient in MySQL, I design and manage optimized databases, ensuring efficient data storage, retrieval, and performance tuning.
+                </p> 
+              </div>
+            </div>
+            <div className="grid grid-cols-2 p-8">
+              <div
+                className="block rounded-lg p-6 text-surface shadow-secondary-1 dark:bg-surface-dark text-white bg-gray-700 border border-gray-500">
+                <h5 className="mb-2 text-xl font-medium leading-tight">Laravel</h5>
+                <p className="mb-4 text-base">
+                  Experienced in Laravel, I develop maintainable web applications with powerful backend functionality and API integrations.
+                </p> 
+              </div>
+              <div
+                className="block rounded-lg p-6 text-surface shadow-secondary-1 dark:bg-surface-dark text-white bg-gray-700 border border-gray-500">
+                <h5 className="mb-2 text-xl font-medium leading-tight">Video Editing</h5>
+                <p className="mb-4 text-base">
+                  Skilled in video editing, I create engaging and high-quality content, enhancing visuals with smooth transitions, effects, and storytelling                
+                </p> 
+              </div>
+            </div>
+            <div className="grid grid-cols-2 p-8">
+              <div
+                  className="block rounded-lg p-6 text-surface shadow-secondary-1 dark:bg-surface-dark text-white bg-gray-700 border border-gray-500">
+                  <h5 className="mb-2 text-xl font-medium leading-tight">PHP</h5>
+                  <p className="mb-4 text-base">
+                    I specialize in PHP development, building dynamic and scalable web applications with clean, efficient code and best practices.
+                  </p> 
+                </div>
+                <div
+                  className="block rounded-lg p-6 text-surface shadow-secondary-1 dark:bg-surface-dark text-white bg-gray-700 border border-gray-500">
+                  <h5 className="mb-2 text-xl font-medium leading-tight">Scrum</h5>
+                  <p className="mb-4 text-base">
+                    Skilled in applying Scrum methodologies to drive agile development, facilitating Sprint Planning, Daily Stand-ups, Reviews, and Retrospectives.                
+                  </p> 
+                </div>
+            </div>
+        </div>
       </div>      
   );
 }
